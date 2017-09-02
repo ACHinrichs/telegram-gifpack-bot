fd9adaa46e63bad32f64ba31361ff03e071bac74
ACHinrichs <adrian-corin.hinrichs@gmx.de>
2017-09-02 13:21:33 +0200

    Do things

--- bot.py
+++ bot.py
@@ -22,6 +22,12 @@ logger = logging.getLogger(__name__)
 HELPTEXT='''
 Type /newPack to start a new Pack
 '''
+ERROR_MSG='''
+Unfortunatly, there was an error.
+Please try again Later.
+
+If the error occurs often, please add a new Bugreport at the [Issuetracker](https://github.com/ACHinrichs/telegram-gifpack-bot/issues)
+'''
 
 dataFile="data.pkl"
 data = {}
@@ -32,8 +38,19 @@ data["next_pack_id"] = 0
 gifpacks = {}
 
 
+admin_token=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
+class InvalidIDException(Exception):
+    """Exception raised for invalid Pack-IDs.
 
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
 
 class States(Enum):
     NONE=0
@@ -42,19 +59,67 @@ class States(Enum):
     NEW_PACK_ADD_TEXT=2
 
 class Gif:
-    def __init(self, gif_id, thumb_id, text):
+    def __init__(self, gif_id, thumb_id, text):
         self.gif_id=gif_id
         self.thumb_id=thumb_id
         self.text=text
 
 
-admin_token=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+class GifPack:
+    gifs=[]
+    name=""
+    creator=-1
+    pack_id=-1
+    
+    def __init__(self, name, creator, pack_id=-1, gifs=[]):
+        self.gifs=gifs
+        self.name=name
+        self.creator=creator
+        self.pack_id=pack_id
+        
+    def add_gif(self, gif, thumb, text):
+        self.gifs.append(Gif(gif, thumb, text))
+
+    def set_id(self, pack_id):
+        self.pack_id=pack_id
+
+        
+    def set_name(self, name):
+        self.name=name
 
 
+class GifPackCollection:
+
+    # pack_path: String the path, where to store the packs WITHOUT following /
+    def __init__(self, pack_path):
+        self.gif_packs={}
+        self.pack_path=pack_path
+
+
+    def add_pack(self, gif_pack):
+        if not (gif_packs[gif_pack.pack_id]):
+            self.gif_packs[gif_pack.pack_id]=gif_pack
+            save_obj(self.gif_pack,self.pack_path+"/"+gif_pack.pack_id+".pkl")
+        else:
+            raise InvalidIDException("GifPackCollection.add_pack", "pack-id "+
+                                     gif_pack.pack_id+" is already taken")
+
+    def get_pack(self, pack_id):
+        if (self.gif_packs[pack_id]):
+            return self.gif_packs[pack_id]
+        else:
+            try:
+                pack=load_obj(self.pack_path+"/"+pack_id+".pkl")
+                self.gif_packs[pack_id]=pack
+                return pack
+            except Exception as e:
+                alert(e)
+                return None
+                
 def save_obj(obj, name ):
     with open(name, 'w+b') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-        
+
 def load_obj(name ):
     with open(name, 'rb') as f:
         return pickle.load(f)
@@ -69,17 +134,16 @@ def start(bot, update):
 def recieve(bot, update, chat_data):
     message = update.message
     chat_id = message.chat.id
-
     
     if not ("state" in chat_data)  or chat_data["state"]=="none":
         message.reply_text("No command active.\n"+HELPTEXT)
         return
-
+    
     if chat_data["state"]==States.NEW_PACK_NAME:
         if message.text:
-            chat_data["new_pack_name"]=message.text
+            chat_data["new_pack"]=GifPack(message.text, message.chat_id)
             message.reply_text("Ok, please send me the first Gif for your new pack _"+
-                               chat_data["new_pack_name"]+"_!",parse_mode=ParseMode.MARKDOWN)
+                               chat_data["new_pack"].name+"_!",parse_mode=ParseMode.MARKDOWN)
             chat_data["state"]=States.NEW_PACK_ADD_GIF
         else:
             message.reply_text("Error, please add a Name first",
@@ -102,9 +166,7 @@ def recieve(bot, update, chat_data):
                                        parse_mode=ParseMode.MARKDOWN)
             else:
                 if chat_data["state"]==States.NEW_PACK_ADD_TEXT:
-                    chat_data["new_pack"].append({"gif":chat_data["new_gif"],
-                                                  "thumb":chat_data["new_thumb"],
-                                                  "text":message.text})
+                    chat_data["new_pack"].add_gif(chat_data["new_gif"], chat_data["new_thumb"], message.text)
                     message.reply_text("Appended to your Pack.\n"+
                                        "_Send another one to continue, "+
                                        "type _/finish_ to save and publish "+
@@ -115,23 +177,37 @@ def recieve(bot, update, chat_data):
 def newPack(bot, update, chat_data):
     if not ("state" in chat_data) or chat_data["state"]==States.NONE:
         chat_data["state"]=States.NEW_PACK_NAME
-        chat_data["new_pack"]=[]
         update.message.reply_text("Ok, we will create a new Gif-Pack.\n\n"+
                            "Please choose a name for your new pack\n\n"+
                            "_Type _/cancel_ to abort creation_",
                            parse_mode=ParseMode.MARKDOWN)
 
-def aport(bot, update, job_queue):
+def abort(bot, update, chat_data):
     chat_data["state"]=States.NONE
     chat_data["new_pack"]=None
+    update.message.reply_text("Packcreation Aborted",
+                              parse_mode=ParseMode.MARKDOWN)
 
 
-def finish(bot, update, job_queue):
+def finish(bot, update, chat_data):
     chat_data["state"]=States.NONE
-    chat_data["new_pack"]=None
-
     
-def admin(bot, update, job_queue):
+    pack_id=data["next_pack_id"]
+    data["next_pack_id"]=data["next_pack_id"]+1
+    chat_data["new_pack"].set_id(pack_id)
+
+    try:
+        gifpacks[pack_id]=chat_data["new_pack"]
+        chat_data["new_pack"]=None
+        saveData()
+        update.message.reply_text("Pack successfully saved! Aborted",
+                                  parse_mode=ParseMode.MARKDOWN)
+    except Exception as e:
+        alert(e)
+        update.message.reply_text(ERROR_MSG,
+                                  parse_mode=ParseMode.MARKDOWN)
+    
+def admin(bot, update, chat_data):
     global data
     global admin_token
     chat_id=update.message.chat_id
@@ -163,6 +239,9 @@ def test(bot, update,chat_data):
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
+
+def alert(msg):
+    print(msg)
     
 def main():
 
